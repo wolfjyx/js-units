c0f46c745b54a6051de207df48ffdc5d31a17970
wolfjyx <wolfjyx@163.com>
2017-08-08 10:11:02 +0800

    add

--- event_handle.js
+++ event_handle.js
@@ -36,5 +36,50 @@ var EventUntil = {
         }else{
             event.cancelBubble();
         }
+    },
+    getRelatedTarget: function (event) {
+        if(event.relatedTarget){
+            return event.relatedTarget;
+        } else if(event.toElement){
+            return event.toElement;
+        }else if(event.fromElement){
+            return event.fromElement;
+        }else {
+            return null;
+        }
+    },
+    getButton:function (event) {
+        if(document.implementation.hasFeature('MouseEvent','2.0')){
+            return event.button;
+        } else {
+            switch (event.button) {
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                    break;
+                case 2:
+                case 6:
+                    return 2;
+                case 4:
+                    return 1;
+            }
+        }
+    },
+    getWheelDelta: function (event) {
+        if(event.wheelDelta){
+            return (client.engine.opera && client.engine.opera <9.5 ? -event.wheelDelta : event.wheelDelta)
+        } else {
+            return -event.detail * 40;
+        }
+    },
+    getCharCode: function (event) {
+        if(typeof event.charCode == 'number'){
+            return event.charCode;
+        } else {
+            return event.keyCode;
+        }
     }
-}
+};
